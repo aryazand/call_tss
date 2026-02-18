4bea3e3a0ef82d8bc7dae75e3541ff0c7f91ab71
Arya Zandvakili <28754724+aryazand@users.noreply.github.com>
2026-02-17 22:06:56 -0600

    print variables for troubleshooting

--- workflow/scripts/bed_to_bw.R
+++ workflow/scripts/bed_to_bw.R
@@ -10,5 +10,12 @@ seqinfo(fiveprime_bed) <- Seqinfo(seqnames = fai[,1], seqlengths = fai[,2])
 
 # Create BigWig Object
 w = ifelse(length(fiveprime_bed) > 0, fiveprime_bed$score, 0)
+
+print(fiveprime_bed)
+print(w)
+
 fiveprime_coverage <- coverage(fiveprime_bed, weight = w)
+
+print(fiveprime_coverage)
+
 export.bw(fiveprime_coverage, snakemake@output[[1]])
